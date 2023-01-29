c753b82aee7b6f46da1da398f4f0a2f0c8d76351
Strykez <120178109+Strykez@users.noreply.github.com>
2023-01-29 10:05:46 -0800

    Added Admin Editing

--- admin/misc_scripts/save_admin.php
+++ admin/misc_scripts/save_admin.php
@@ -9,8 +9,24 @@ if (!isset($_POST['name']) || !isset($_POST['email']) || !isset($_POST['role']))
     return;
 }
 
+require_once "../../config.php";
+
+
+header("HTTP/1.1 204 No Content");
+
+$id = $_POST['id'];
 $name = $_POST['name'];
 $email = $_POST['email'];
 $role = $_POST['role'];
 
-?>
+editUser($id, $email, $name, $role, $db);
+
+function editUser($id, $email, $name, $role, $db)
+{
+    $edit_stmt = mysqli_prepare($db, "UPDATE users SET email=?, name=?, role=?, WHERE id=?  (id, email, name, pass, role) VALUES (NULL, ?, ?, ?, ?)");
+    mysqli_stmt_bind_param($edit_stmt, 'sssi', $email, $name, $role, $id);
+    mysqli_stmt_execute($edit_stmt);
+}
+
+header("Location: ".$_SERVER['HTTP_REFERER']);
+exit;
